d60a3464cfa544168d053880bcb73fa0fc343333
yunCrush <yunfei1577266641@gmail.com>
2022-03-25 16:59:57 +0800

    json使用

---
+++ src/main/java/yun/juc/json/JSONTest.java
@@ -0,0 +1,9 @@
+package yun.juc.json;
+
+/**
+ * Author: yunCrush
+ * Date: 2022/3/25 15:42
+ * Description:
+ */
+public class JSONTest {
+}
---
+++ src/main/java/yun/juc/json/PersonEntity.java
@@ -0,0 +1,9 @@
+package yun.juc.json;
+
+/**
+ * Author: yunCrush
+ * Date: 2022/3/25 15:43
+ * Description:
+ */
+public class PersonEntity {
+}
